919e47e50bbbfdef9f9a5ad5a4919cc36b1b6058
Chris Romp <ChrisRomp@users.noreply.github.com>
2025-05-20 18:34:40 +0000

    Fix WPF restore on linux so linter can run - Add local linter script - Re-enable SLN analyzers in linter action - Add EnableWindowsTargeting to WPF csproj file

---
+++ scripts/run-linter-locally.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker run --rm \
+  -e LOG_LEVEL=INFO \
+  -e RUN_LOCAL=true \
+  -e DEFAULT_BRANCH=$(git branch --show-current) \
+  -e VALIDATE_DOTNET_SLN_FORMAT_ANALYZERS=true \
+  -e FIX_DOTNET_SLN_FORMAT_ANALYZERS=true \
+  -e VALIDATE_DOTNET_SLN_FORMAT_STYLE=true \
+  -e FIX_DOTNET_SLN_FORMAT_STYLE=true \
+  -e VALIDATE_DOTNET_SLN_FORMAT_WHITESPACE=true \
+  -e FIX_DOTNET_SLN_FORMAT_WHITESPACE=true \
+  -e VALIDATE_YAML_PRETTIER=true \
+  -e FIX_YAML_PRETTIER=true \
+  -v $CODESPACE_VSCODE_FOLDER:/tmp/lint \
+  -v $CODESPACE_VSCODE_FOLDER/../super-linter-output:/tmp/super-linter-output \
+  ghcr.io/super-linter/super-linter:latest
